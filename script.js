88248ce14a310932be2539f6cb6dc350509289cb
Spencer Berggreen <spencerberggreen@gmail.com>
2022-05-21 19:08:39 -0500

    resize buttons

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 // user chooses rock, paper, or scissors
-
 // computer chooses at random
-
 // decide who wins
-
 // tell user who wins
+
+const computerChoice = document.getElementById('computer-choice');
+const userChoice = document.getElementById('user-choice');
+const result = document.getElementById('result');
